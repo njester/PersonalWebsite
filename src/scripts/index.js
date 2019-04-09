708d25be923af14175b38720bab7ef0cea18a8c4
Manuel <njester5030@gmail.com>
2019-04-09 21:11:53 +0200

    site made ie11 compatible

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,30 @@
+import cssVars from 'css-vars-ponyfill';
+require('intersection-observer');
+// Call using defaults
+cssVars({
+  rootElement  : document,
+  shadowDOM    : false,
+  include      : 'link[rel=stylesheet],style',
+  exclude      : '',
+  variables    : {},
+  fixNestedCalc: true,
+  onlyLegacy   : true,
+  onlyVars     : false,
+  preserve     : false,
+  silent       : false,
+  updateDOM    : true,
+  updateURLs   : true,
+  watch        : false});
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
+
 import '../styles/index.scss';
 
 // import './nav.js';
